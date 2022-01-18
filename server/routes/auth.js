84d6078d5c5e840e6473d5930f3d2cae2ee1da52
MikelGV <mikelgaldosv@gmail.com>
2022-01-18 09:04:18 +0100

    register added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
-router.get("/", (req, res) => {
-    res.send("hey its auth route")
+//Register
+router.post("/register", async (req, res) => {
+
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedpwd = await bcrypt.hash(req.body.password, salt)
+        //create new user
+        const newUser = await new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashedpwd
+        });
+
+        // save user and respond
+        const user = await newUser.save()
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 module.exports = router
