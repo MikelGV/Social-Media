42a67ef71773ac147bc2288dbb705feb75209c7f
MikelGV <mikelgaldosv@gmail.com>
2022-01-18 23:01:25 +0100

    post added

--- client/src/components/feed/feed.jsx
+++ client/src/components/feed/feed.jsx
@@ -1,11 +1,13 @@
 import "./feed.css"
 import Share from "../share/share"
+import Post from "../post/post"
 
 export default function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
+                <Post />
             </div>
         </div>
     );
