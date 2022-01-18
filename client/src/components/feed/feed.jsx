19cde26dec965f7d6a988f8cce33b46cfe259732
MikelGV <mikelgaldosv@gmail.com>
2022-01-18 22:16:34 +0100

    style added

---
+++ client/src/components/feed/feed.jsx
@@ -0,0 +1,12 @@
+import "./feed.css"
+import Share from "../share/share"
+
+export default function Feed() {
+    return (
+        <div className="feed">
+            <div className="feedWrapper">
+                <Share />
+            </div>
+        </div>
+    );
+}
