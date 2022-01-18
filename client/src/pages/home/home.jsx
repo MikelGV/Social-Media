99374208af38bd3040515b3c2a06607467a0ed67
MikelGV <mikelgaldosv@gmail.com>
2022-01-18 21:44:52 +0100

    homeContainer added

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import Topbar from "../../components/topbar/topbar";
 import Sidebar from "../../components/sidebar/sidebar";
 import Rightbar from "../../components/rightbar/rightbar";
 import Feed from "../../components/feed/feed";
+import "./home.css"
 
 
 export default function Home() {
