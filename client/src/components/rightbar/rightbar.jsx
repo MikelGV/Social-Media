62e6898f4203b2d8ab4f0408be48f6564a4ce16a
MikelGV <mikelgaldosv@gmail.com>
2022-01-19 18:15:41 +0100

    rightbar added

--- client/src/components/rightbar/rightbar.jsx
+++ client/src/components/rightbar/rightbar.jsx
@@ -3,7 +3,58 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/gift.png" alt="" />
+                    <span className="birthdayText"><b>Pola Foster</b> and <b>3 other frineds</b> have a birthday today.</span>
+                </div>
+                <img className="rightbarAd" src="/assets/ads.jpg" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="righbarFrinedList">
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="righbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="/assets/person/pexels-koolshooters-7689011.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Joe mama</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
