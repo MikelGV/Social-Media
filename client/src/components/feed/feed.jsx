f12ea039c043b571d4bdd812ca1f1e4145a54c90
MikelGV <mikelgaldosv@gmail.com>
2022-01-20 05:06:40 +0100

    things updated and multer added

--- client/src/components/feed/feed.jsx
+++ client/src/components/feed/feed.jsx
@@ -1,23 +1,27 @@
 import "./feed.css"
 import Share from "../share/share"
 import Post from "../post/post"
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useEffect } from "react";
 import axios from "axios"
+import {AuthContext} from "../../context/AuthContext"
 
 
 export default function Feed({username}) {
-    const[posts, setPosts] = useState([])
+    const[posts, setPosts] = useState([]);
+    const {user} = useContext(AuthContext);
 
     useEffect(()=> {
         const fetchPosts = async () => {
             const res = username 
                 ? await axios.get("/posts/profile/" + username) 
-                : await axios.get("/posts/timeline/61e68179558a025afee931da")
-            setPosts(res.data)
+                : await axios.get("/posts/timeline/" + user._id)
+            setPosts(res.data.sort((p1,p2)=>{
+                return new Date(p2.createdAt) - new Date(p1.createdAt);
+            }))
         }
         fetchPosts();
-    },[username])
+    },[username, user._id])
     return (
         <div className="feed">
             <div className="feedWrapper">
--- client/src/components/share/share.jsx
+++ client/src/components/share/share.jsx
@@ -5,22 +5,53 @@ import {
     Room,
     EmojiEmotions
 } from "@mui/icons-material";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useRef, useState } from "react";
+import axios from "axios";
 
-export default function share() {
+export default function Share() {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const {user} = useContext(AuthContext);
+    const desc = useRef()
+    const [file, setFile] = useState(null)
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const newPost = {
+          userId: user._id,
+          desc: desc.current.value,
+        };
+        if (file) {
+          const data = new FormData();
+          const fileName = Date.now() + file.name;
+          data.append("name", fileName);
+          data.append("file", file);
+          newPost.img = fileName;
+          console.log(newPost);
+          try {
+            await axios.post("/upload", data);
+          } catch (err) {}
+        }
+        try {
+          await axios.post("/posts", newPost);
+          window.location.reload();
+        } catch (err) {}
+      };
     return (
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img className="shareProfileImage" src="/assets/person/1.jpg" alt="" />
-                    <input placeholder="What's in your mind Mikel?" className="shareInput" />
+                    <img className="shareProfileImage" src={user.profilePicture ? PF+user.profilePicture :PF+"person/noAvatar.png"} alt="" />
+                    <input placeholder={"What's in your mind "+user.username+"?"} className="shareInput" ref={desc} />
                 </div>
                 <hr className="shareHr" />
-                <div className="shareBottom">
+                <form className="shareBottom" onSubmit={submitHandler}>
                     <div className="shareOptions">
-                        <div className="shareOption">
+                        <label htmlFor="file" className="shareOption">
                             <PermMedia htmlColor="tomato" className="shareIcon" />
                             <span className="shareOptionText">Photo or Video</span>
-                        </div>
+                            <input style={{display:"none"}} type="file" id="file" accept=".png,.jpg,.jpeg" onChange={(e) =>setFile(e.target.files[0])} />
+                        </label>
                         <div className="shareOption">
                             <Label htmlColor="blue" className="shareIcon" />
                             <span className="shareOptionText">Tag</span>
@@ -34,8 +65,8 @@ export default function share() {
                             <span className="shareOptionText">Feelings</span>
                         </div>
                     </div>
-                    <button className="shareButton">Share</button>
-                </div>
+                    <button className="shareButton" type="submit">Share</button>
+                </form>
             </div>
         </div>
     )
