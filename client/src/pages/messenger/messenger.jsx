65f2c17f2fb10917fb0fda8f6f9d1b01bc3eb439
MikelGV <mikelgaldosv@gmail.com>
2022-01-24 07:45:54 +0100

    chatMenuWrapper update

--- client/src/pages/messenger/messenger.jsx
+++ client/src/pages/messenger/messenger.jsx
@@ -1,5 +1,25 @@
 import "./messenger.css"
+import Topbar from "../../components/topbar/topbar";
+import Conversations from "../../components/conversations/conversations";
 
 export default function Messenger() {
-  return <div></div>;
+  return (
+      <>
+        <Topbar/>
+        <div className="messenger">
+            <div className="chatMenu">
+                <div className="chatMenuWrapper">
+                    <input placeholder="Search for friends" className="chatMenuInput" />
+                    <Conversations/>
+                </div>
+            </div>
+            <div className="chatBox">
+                <div className="chatBoxWrapper"></div>
+            </div>
+            <div className="chatOnline">
+                <div className="chatOnlineWrapper"></div>
+            </div>
+        </div>
+      </>
+  );
 }
