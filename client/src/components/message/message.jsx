93e003e08c09009343b0e5de99d88ac87c5b2b4b
MikelGV <mikelgaldosv@gmail.com>
2022-01-24 09:18:49 +0100

    message component

--- client/src/components/message/message.jsx
+++ client/src/components/message/message.jsx
@@ -2,6 +2,12 @@ import "./message.css"
 
 export default function Message() {
   return (
-      <div></div>
+      <div className="message">
+          <div className="messageTop">
+              <img src="" className="messageImg" alt="" />
+              <p className="messageText">Hello this is a message</p>
+          </div>
+          <div className="messageBottom">1hour ago</div>
+      </div>
   );
 }
