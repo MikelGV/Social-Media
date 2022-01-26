7d041f0b0ccd99ee12f9cbe76c5dcd33aed072de
MikelGV <mikelgaldosv@gmail.com>
2022-01-26 02:48:49 +0100

    logic added to message

--- client/src/components/message/message.jsx
+++ client/src/components/message/message.jsx
@@ -1,13 +1,14 @@
 import "./message.css"
+import {format} from "timeago.js"
 
-export default function Message({own}) {
+export default function Message({message, own}) {
   return (
       <div className={own ? "message own" : "message"}>
           <div className="messageTop">
-              <img src="" className="messageImg" alt="" />
-              <p className="messageText">Hello this is a message</p>
+              <img src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="messageImg" alt="" />
+              <p className="messageText">{message.text}</p>
           </div>
-          <div className="messageBottom">1hour ago</div>
+          <div className="messageBottom">{format(message.createdAt)}</div>
       </div>
   );
 }
