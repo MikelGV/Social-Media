7599588f46a735ac68b61cce2eec1f7a1a3a0dba
Mikel <mikelgaldosv@gmail.com>
2022-05-16 02:38:41 +0200

    trying to deploy the app

--- server/app.js
+++ server/app.js
@@ -55,9 +55,12 @@ app.use("/api/conversations", conversationRouter);
 app.use("/api/messages", messageRouter)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('../client/build'));
+    app.use(express.static(path.resolve(__dirname, "../client/build")));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    })
 }
 
 app.listen(PORT, ()=>{
-    console.log("Server running")
+    console.log(`Server running on port ${PORT}`)
 });
