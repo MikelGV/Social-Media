cbd101721dbf51afbc17c9f5bdb9e95d89af8f75
Mikel <mikelgaldosv@gmail.com>
2022-08-31 04:09:52 +0200

    things deleted

--- server/app.js
+++ server/app.js
@@ -54,19 +54,6 @@ app.use("/api/posts", postRouter);
 app.use("/api/conversations", conversationRouter);
 app.use("/api/messages", messageRouter)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-    app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
-        ),
-    )
-} else {
-    app.get('/', (req, res) => {
-        res.send('Hello from app Engine!')
-    })
-}
-
 app.listen(PORT, ()=>{
     console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
 });
